c2e74ec45559b321269f76412facaeadfcb7f3fb
r4t1n <r4t1n@proton.me>
2024-03-07 14:37:50 +0100

    logging off

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,8 +1,6 @@
 extends Node2D
 
 const ACCELERATION: float = 0.1
-const MIN_SPAWN_INTERVAL_SECONDS: float = 0.5
-const MAX_SPAWN_INTERVAL_SECONDS: float = 3.0
 
 var is_playing: bool = false
 var speed: float = 8
@@ -10,6 +8,7 @@ var speed: float = 8
 @onready var level: ParallaxBackground = $Level
 @onready var level_parallax_layer: ParallaxLayer = $Level/ParallaxLayer
 @onready var cactus_scene: PackedScene = preload("res://scenes/cactus.tscn")
+@onready var cactus_timer: Timer = $CactusTimer
 
 var cactus_big_position_y: int = -35
 var cactus_small_position_y: float = -27.5
@@ -18,11 +17,6 @@ func _process(delta):
 	if is_playing:
 		speed += ACCELERATION * delta
 		level.scroll_base_offset.x -= speed
-		var cactus_instance = cactus_scene.instantiate()
-		level_parallax_layer.add_child(cactus_instance)
-
-func calculate_spawn_interval() -> float:
-	return lerp(MIN_SPAWN_INTERVAL_SECONDS, MAX_SPAWN_INTERVAL_SECONDS, speed / 20.0)
 
 func _on_dino_playing():
 	is_playing = true
